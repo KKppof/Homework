f69ad939097f26df9270a92967ce632680aaf319
KKppof <92415411+KKppof@users.noreply.github.com>
2021-11-13 18:31:29 +0800

    Add files via upload

---
+++ length.c
@@ -0,0 +1,25 @@
+/*Determine the length of a message
+#include <stdio.h>
+int main(){
+	char ch;
+	int len = 0;
+	printf("Enter a message:");
+	ch = getchar();
+	while(ch != '\n'){
+		len++;
+		ch = getchar();
+	}
+	printf("Your message was %d character(s) long.\n",len);
+	return 0;
+} 
+*/
+#include <stdio.h>
+int main(){
+	int len = 0;
+	printf("Enter a message:");
+	while(getchar()!='\n'){
+		len++;
+	}
+	printf("Your message was %d character(s) long.\n",len);
+	return 0;
+}
